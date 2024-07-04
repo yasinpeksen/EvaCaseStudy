9e0f6a085c6a736063259d57cdeb4c1a0216b0d2
Hurşit Yasin Pekşen <forgameprogrammer@gmail.com>
2024-07-04 05:20:30 +0300

    feat: Update share controller

--- src/controllers/share.controller.js
+++ src/controllers/share.controller.js
@@ -1,3 +1,13 @@
-export function update(req, res) {
-  
+import * as shareService from "../services/share.service";
+import shareUpdateSchema from "../validations/share-update-request.schema";
+
+function updatePrice(req, res) {
+  const { error, value } = shareUpdateSchema.validate(req.body);
+  if (error) {
+    throw new ValidationException(error.message);
+  }
+
+  shareService.updatePrice(value.symbol, value.price);
 }
+
+export { updatePrice };
