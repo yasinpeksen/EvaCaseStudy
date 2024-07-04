8b3afc9a1dd7fa3fea8d006f8cd105e3b1446e37
Hurşit Yasin Pekşen <forgameprogrammer@gmail.com>
2024-07-04 05:46:08 +0300

    fix: Model file issue

--- src/models/index.js
+++ src/models/index.js
@@ -1,22 +1,27 @@
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const db = {};
+import fs from "fs";
+import path from "path";
+import Sequelize from "sequelize";
 
-const sequelize = new Sequelize({
+const basename = path.basename(__filename);
+
+const sequelize = new Sequelize.Sequelize({
   dialect: "sqlite",
   storage: "database.sqlite",
 });
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
+  .filter((filename) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      filename.length() > 3 &&
+      filename.indexOf(".") !== 0 &&
+      filename !== basename &&
+      filename.slice(-3) === ".js"
     );
   })
-  .forEach((file) => {
-    require(path.join(__dirname, file))(sequelize);
+  .forEach((filename) => {
+    require(path.join(__dirname, filename))(sequelize);
   });
+
+sequelize.sync();
 
 export { sequelize, Sequelize };
